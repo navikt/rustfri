a7bb117567f0a1769ef3702c35dfe73d243b2449
david steinsland <david@davidsteinsland.net>
2023-09-24 19:06:30 +0200

    syntaksjusteringer

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,24 @@ use std::net::{TcpListener, TcpStream};
 fn main() {
     println!("====================\nRUSTFRI\n====================\n");
     match TcpListener::bind("0.0.0.0:8080") {
-        Ok(listener) => { håndter_requests(listener) }
+        Ok(listener) => håndter_requests(&listener),
         Err(error) => panic!("Error: {:?}", error)
     };
 }
 
-fn håndter_requests(listener: TcpListener) {
+fn håndter_requests(listener: &TcpListener) {
     println!("Klar for å motta requests");
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => { håndter_request(stream) }
-            Err(error) => { panic!("Error: {:?}", error) }
+            Ok(stream) => håndter_request(stream),
+            Err(error) => panic!("Error: {:?}", error)
         }
     }
 }
 
 fn håndter_request(mut stream: TcpStream) {
     println!("Connection established! 2");
-    let reader = BufReader::new(&mut stream);
+    let reader = BufReader::new(&stream);
     let request: Vec<String> = reader.lines().filter_map(|line| line.ok()).take_while(|line| !line.is_empty()).collect();
     println!("Request: {:#?}", request);
     let response = "HTTP/1.1 200 OK\r\n\r\nRustfri!";
