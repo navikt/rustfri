2aa7f732cea515bf5fe9331e06ede2f48839c081
david steinsland <david@davidsteinsland.net>
2023-09-25 09:51:18 +0200

    logger meldinger på logstash-kompatibelt jsonformat

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 extern crate core;
 
+use json::{JsonValue, object};
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use chrono::{DateTime, Utc};
+use std::time::SystemTime;
+
 
 fn main() {
-    println!("====================\nRUSTFRI\n====================\n");
+    log_info("====================\nRUSTFRI\n====================\n");
     match TcpListener::bind("0.0.0.0:8080") {
         Ok(listener) => motta_tilkoblinger(&listener),
         Err(error) => panic!("Error: {:?}", error)
@@ -12,7 +16,7 @@ fn main() {
 }
 
 fn motta_tilkoblinger(listener: &TcpListener) {
-    println!("Klar for å motta requests");
+    log_info("Klar for å motta requests");
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => les_og_svar_httpforespørsel(stream),
@@ -22,9 +26,9 @@ fn motta_tilkoblinger(listener: &TcpListener) {
 }
 
 fn les_og_svar_httpforespørsel(stream: TcpStream) {
-    println!("Connection established! 2");
+    log_info("Connection established!");
     let request = les_httpforespørsel(&stream);
-    println!("Request: {:#?}", request);
+    log_info(&format!("Request: {:#?}", request));
     svar_httpforespørsel(&stream)
 }
 
@@ -40,4 +44,24 @@ fn les_httpforespørsel(stream: &TcpStream) -> Vec<String> {
 fn svar_httpforespørsel(mut stream: &TcpStream) {
     let response = "HTTP/1.1 200 OK\r\n\r\nRustfri!";
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn log_info(melding: &str) {
+    println!("{}", json::stringify(lag_logstash_melding("INFO", melding)));
+}
+
+fn lag_logstash_melding(nivå: &str, melding: &str) -> JsonValue {
+    return object!{
+        "@timestamp": tidspunkt_som_rfc3339(),
+        "@version": String::from("1.0"),
+        message: String::from(melding),
+        logger_name: String::from("rustfri"),
+        level: String::from(nivå)
+    };
+}
+
+fn tidspunkt_som_rfc3339() -> String {
+    let tidspunkt_nå = SystemTime::now();
+    let tidspunkt_nå: DateTime<Utc> = tidspunkt_nå.into();
+    return tidspunkt_nå.to_rfc3339();
 }
